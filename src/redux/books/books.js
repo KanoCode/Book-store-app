96ad5ce2e3726c4efcf585c55d0198b01ae18f22
KanoCode <kanokanombola@gmail.com>
2022-06-07 21:03:23 +0200

    Add reducers, actions and redux store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,31 @@
 // widgets.js
 
 // Actions
-const ADD_BOOK = "bookstore/ADD_BOOK";
-const REMOVE_BOOK = "bookstore/REMOVE_BOOK";
-
+const ADD_BOOK = 'bookstore/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/REMOVE_BOOK';
 
 // Reducer
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case "ADD_BOOK":
+    case ADD_BOOK:
       return [...state, action.book];
-    case "REMOVE_BOOK":
-        return state.filter(book => book != action.book)
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.book.id);
     default:
       return state;
   }
 }
 
 // Action Creators
-export function addBook(book) {
-  return { type: "ADD_BOOK", book };
+export function addBook(id, title, author) {
+  return {
+    type: 'ADD_BOOK',
+    book: {
+      id, title, author,
+    },
+  };
 }
 
 export function removeBook(book) {
-  return { type: "REMOVE_BOOK",book };
+  return { type: 'REMOVE_BOOK', book };
 }
-
