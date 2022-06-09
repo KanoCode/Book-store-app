f456ab205c5c80e69b0baeaa5af7469773316eaa
KanoCode <kanokanombola@gmail.com>
2022-06-09 22:04:46 +0200

    Implement Load data on application load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import NavBar from './components/Navbar';
 import './App.css';
 import Categories from './components/pages/Categories';
+import { loadBooks } from './redux/books/books';
 import BooksPage from './components/pages/Default';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(loadBooks());
   return (
     <>
       <NavBar />
